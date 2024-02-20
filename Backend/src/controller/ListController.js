604ea85baeea4d5a82c0054db49016d350c4064f
unknown <abbasch1414@gmail.com>
2024-02-21 01:45:09 +0500

    added update in todo task and update router for updation

--- Backend/src/controller/ListController.js
+++ Backend/src/controller/ListController.js
@@ -5,6 +5,7 @@ const TodoListController = {
     try {
       const { task } = req.body;
       await TodoListModel.create({
+        userId:req.session.user.id,
         task,
       });
       res.status(200).json({ Message: "Task created." });
@@ -15,8 +16,24 @@ const TodoListController = {
     }
   },
   updateTask:async(req,res)=>{
-
-  }
+        try {
+          const { id } = req.params;
+          const {task}=req.body;
+          const TodoTask = await TodoListModel.findOne({
+            where: { id },
+          });
+          if (TodoTask) {
+            TodoTask.task = task;
+            TodoTask.save(res.json({ message: "Task updated successfully." }));
+          } else {
+            res.status(400).json({ message: "Task not found for updation" });
+          }
+        } catch (error) {
+          res
+            .status(404)
+            .json({ message: "Something bad happend in task updation." });
+        }
+      }
 };
 
 export default TodoListController;
--- Backend/src/routes/routes/ListRouter.js
+++ Backend/src/routes/routes/ListRouter.js
@@ -4,6 +4,7 @@ import TodoListController from "../../controller/ListController.js";
 const TodoListRouter = Router()
 
 TodoListRouter.post("/create",TodoListController.taskCreation)
+TodoListRouter.put("/update/:id",TodoListController.updateTask)
 
 
 export default TodoListRouter;
